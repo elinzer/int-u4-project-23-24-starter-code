34ed9bb3b248a711f4cbcb1256e744254ca80045
El Linzer <101808290+elinzer@users.noreply.github.com>
2024-03-18 18:41:50 +0000

    most of the html done, started JS on demo quiz

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// JavaScript goes here.
+let firstQuestion = document.querySelector(".question1")
+let choice1 = document.querySelector(".choice1");
+let choice2 = document.querySelector(".choice2");
+
+let secondQuestion = document.querySelector(".question2")
+
+
+let answer1;
+let answer2;
+
+
+choice1.addEventListener("click", function(){
+  
+  answer1 = 1;
+  firstQuestion.style.display = "none";
+  secondQuestion.style.display = "block";
+  
+})
+
+choice2.addEventListener("click", function(){
+
+  answer1 = 2;
+  firstQuestion.style.display = "none";
+  secondQuestion.style.display = "block";
+
+})
