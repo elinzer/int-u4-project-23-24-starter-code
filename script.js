564b288072d91ee1b8c023d9ee3a38d8afa3db3f
El Linzer <101808290+elinzer@users.noreply.github.com>
2024-03-20 20:07:02 +0000

    changed logic of moving through questions

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
+let startImage = document.querySelector(".image-start");
+let h3 = document.querySelector("h3");
+let allQuestions = document.querySelector(".questions");
+let result = document.querySelector(".result");
+
+// first question variables
 let firstQuestion = document.querySelector(".question1")
 let choice1 = document.querySelector(".choice1");
 let choice2 = document.querySelector(".choice2");
 
+//second question variables
 let secondQuestion = document.querySelector(".question2")
+let choice3 = document.querySelector(".choice3");
+let choice4 = document.querySelector(".choice4");
 
-
+//variables that store the users answers
 let answer1;
 let answer2;
+
+startImage.addEventListener("click", function(){
+
+  h3.style.display = "none";
+  startImage.style.display = "none";
+  allQuestions.style.display = "block";
+  firstQuestion.style.display = "block";
+
+})
 
 
 choice1.addEventListener("click", function(){
   
   answer1 = 1;
-  firstQuestion.style.display = "none";
-  secondQuestion.style.display = "block";
+  secondQuestion.scrollIntoView()
   
+
 })
 
 choice2.addEventListener("click", function(){
 
   answer1 = 2;
-  firstQuestion.style.display = "none";
   secondQuestion.style.display = "block";
+
+})
+
+choice3.addEventListener("click", function(){
+
+  answer2 = "friends";
+  
+  secondQuestion.style.display = "none";
 
 })
